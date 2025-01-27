46098e37da38ab7c90c76696fdb32ea41cc60ef8
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2025-01-27 15:23:41 +0530

    change in protected route

--- src/common/utilities/protectedRoute.ts
+++ src/common/utilities/protectedRoute.ts
@@ -1,13 +1,13 @@
-import store from "../store/store";
-import { Navigate } from "react-router-dom"
+// import store from "../store/store";
+// import {Navigate} from "react-router-dom"
 
 
 const ProtectedRoutes = ({ children }: any) => {
-   const { auth } = store.getState()
-   const token = localStorage.getItem("token")
-   if (token) {
-      return Navigate({ to: "/" })
-   }
+   // const {auth}=store.getState()
+   // const token=localStorage.getItem("token")
+   // if(token){
+   //  return Navigate({to:"/"})
+   // }
    return children
 
 }
