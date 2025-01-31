fc7889f8df8f0e45d12db200585ca9ce0750d7cc
techyyhimanshu <158696334+techyyhimanshu@users.noreply.github.com>
2025-01-31 11:23:08 +0530

    changed base url

--- src/api/base-url.ts
+++ src/api/base-url.ts
@@ -1,3 +1,4 @@
-const baseUrl = "http://localhost:3300"
+// const baseUrl = "http://localhost:3300"
 // const baseUrl = "http://api.creditbricks.in:3300"
+const baseUrl = "http://18.60.70.52:3300"
 export default baseUrl
