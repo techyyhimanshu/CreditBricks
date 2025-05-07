5bab1f5fa6c9ce09a8e4ef5e2d513c9886d8575d
khushboo <>
2025-05-07 13:57:40 +0530

    search society in header with scroll bar design

--- src/layout/layoutcomponent/header.tsx
+++ src/layout/layoutcomponent/header.tsx
@@ -1,6 +1,6 @@
 import { Fragment, useState, useEffect } from "react";
-import { Navbar, Dropdown, OverlayTrigger, Tooltip } from "react-bootstrap";
-// import PerfectScrollbar from 'react-perfect-scrollbar';
+import { Navbar, Dropdown, OverlayTrigger, Tooltip, Form } from "react-bootstrap";
+import PerfectScrollbar from 'react-perfect-scrollbar';
 import { Link } from "react-router-dom";
 import { imagesData } from "../../common/commonimages";
 import * as Switcherdata from '../../common/switcherdata';
@@ -477,7 +477,49 @@ export default function Header() {
         </> */}
                   </li>
                   <li className="dropdown nav-item w-auto headericon me-3">
-                      <Dropdown>
+                  <Dropdown className=" nav-item main-header-notification d-flex">
+                  <Dropdown.Toggle className="new nav-link"  variant="">
+                    <i className="bi bi-building"></i>
+                    <i className="bi bi-chevron-down ms-2"></i>
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu className="slid1 society_select">
+                    <div className="p-2 ps-3 text-start border-bottom">
+                      <div className="d-flex">
+
+                        <h6 className="dropdown-title w-100 mb-1 tx-15 font-weight-semibold text-black">
+                        Society List
+                        <Form.Control className="form-control w-100 mt-2" placeholder="Search Society" type="text"></Form.Control>
+                        </h6>
+
+                      </div>
+                   </div>
+
+                    <PerfectScrollbar options={{ suppressScrollX: true, useBothWheelAxes: false }} style={{ height: 300 }}>
+                      <div className="main-notification-list Notification-scroll ">
+
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Mohan Areca Co-Op Housing Society Limited </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">SKA MetroVilla Society Limited </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Testname </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom active">Credit Bricks Pvt. Ltd. </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Signature Global City </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Supertech Limited </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Unitech Group </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Ansal Group </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Mohan Areca Co-Op Housing Society Limited </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">SKA MetroVilla Society Limited </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Testname </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Credit Bricks Pvt. Ltd. </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Signature Global City </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Supertech Limited </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Unitech Group </Dropdown.Item>
+                        <Dropdown.Item className="d-flex p-2 ps-3 border-bottom">Ansal Group </Dropdown.Item>
+                      </div>
+
+                    </PerfectScrollbar>
+
+                  </Dropdown.Menu>
+                </Dropdown>
+                      {/* <Dropdown>
                         <OverlayTrigger
                           placement="bottom"
                           overlay={<Tooltip>Switch Your Society</Tooltip>}>
@@ -491,6 +533,7 @@ export default function Header() {
                         </OverlayTrigger>
                         <Dropdown.Menu className="society_select">
 
+
                           <Dropdown.Item className="dropdown-item" href="/">
                             <i className="bi bi-building me-2"></i>Mohan Areca Co-Op Housing Society Limited
                           </Dropdown.Item>
@@ -499,7 +542,7 @@ export default function Header() {
                             <i className="bi bi-building me-2"></i> SKA MetroVilla Society Limited
                           </Dropdown.Item>
                         </Dropdown.Menu>
-                      </Dropdown>
+                      </Dropdown> */}
                     </li>
                   {
                     userType === "admin" && <li className="dropdown nav-item w-auto headericon me-3">
